19f8154a7b4429c8763af1b46e399018a1c0a3d9
ahmedezzat1 <65311452+ahmedezzat1@users.noreply.github.com>
2021-12-18 00:54:07 +0200

    Create main.cpp

---
+++ Task1/problem1_rectangle/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/***************************************************************/
+
+int main() {
+    
+
+    int  length , width;
+
+    cin>>length>>width;
+
+    cout<<length*width<<endl;
+    cout<<(length+width)*2<<endl;
+
+}
+    
+
+/***************************************************************/
