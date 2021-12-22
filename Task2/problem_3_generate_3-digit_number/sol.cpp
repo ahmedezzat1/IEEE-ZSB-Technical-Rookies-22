7d597ab3b9a802e2f58026175cb8bf236b53cc4d
ahmedezzat1 <65311452+ahmedezzat1@users.noreply.github.com>
2021-12-23 00:32:50 +0200

    Create sol.cpp

---
+++ Task2/problem_3_generate_3-digit_number/sol.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define loop(n)          for(int i = 0; i < (n); i++)
+#define IOS    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/***************************************************************/
+
+int main() {
+   // file();
+    IOS
+
+    srand(time(0));
+   int  random = rand() % 1000+100 ;
+   int n=random;
+   string s= to_string(n);
+   s="427";
+   string x;
+    int cnt=0;
+    do {
+        cin>>x;
+        int hit=0 , miss=0;
+        
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j <3 ; ++j) {
+                if (s[i]==x[i] and i==j)
+                    hit++;
+                else if(s[i]==x[i] and i!=j)
+                    miss++;
+            }
+        }
+        cout<< hit <<"hits" <<" ";
+        cout<< miss <<"misses"<<endl;
+        cnt++;
+    }
+    while (x!=s);
+
+    cout<<cnt<<"guesses"<<endl;
+}
+
+/***************************************************************/
+
+
